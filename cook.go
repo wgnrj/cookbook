3b7320fdb2df47730405a394345dd2b7999230ed
Jochen Wagner <jochen@cookie.camp>
2016-09-30 08:15:04 +0200

    Improved functionality, searching
    Added Search function to get recipes containing given tags.
    Added searchHandler.
    Added main.html to display all recipes and search form and add form.
    Everything works so far, but the regexp for validIngredient is not
    accepting enough.
    Improved recipe and ingredient parsing.
    Added tags to recipes.

--- cook.go
+++ cook.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/wgnrj/search"
+	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"fmt"
-	"io/ioutil"
-	//"encoding/json"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type Ingredient struct {
@@ -15,7 +22,41 @@ type Ingredient struct {
 }
 
 func (i *Ingredient) String() string {
-    return fmt.Sprintf("%v %v %v", i.Amount, i.Unit, i.Name)
+	if i.Amount == 0 {
+		return i.Name
+	}
+	return fmt.Sprintf("%v%v %v", i.Amount, i.Unit, i.Name)
+}
+
+// Uninterpreted/raw string literal
+var validIngredient = regexp.MustCompile(`^((\d+([,\.]\d+)?)([a-zA-Z]*))? ?([A-Za-z0-9äöüÄÖÜß\(\)\- ]+)$`)
+
+func Ingredients(s string) ([]Ingredient, error) {
+	var ingredients []Ingredient
+	for _, v := range strings.Split(s, "\n") {
+		v := strings.Trim(v, "\r\n")
+		if v != "" {
+			m := validIngredient.FindStringSubmatch(v)
+			if m == nil {
+				return nil, errors.New("No match")
+			}
+			var f float64
+			var err error
+			if m[2] != "" {
+				f, err = strconv.ParseFloat(strings.Replace(m[2], ",", ".", 1), 64)
+				if err != nil {
+					return nil, err
+				}
+			}
+			ingredients = append(ingredients, Ingredient{
+				Name:   m[5],
+				Amount: f,
+				Unit:   m[4],
+			})
+		}
+	}
+
+	return ingredients, nil
 }
 
 type Recipe struct {
@@ -24,50 +65,199 @@ type Recipe struct {
 	Steps       []string
 	Image       string
 	Source      string
+	Tags        []string
 }
 
 func (r *Recipe) String() string {
-    var i, s string
-    for _, v := range r.Ingredients {
-        i += "\n"+v.String()
-    }
-    for _, v := range r.Steps {
-        s += "\n\n"+v
-    }
-    return fmt.Sprintf(fmt.Sprintf("%v\n%v%v\n\n%v\n%v", r.Title, i, s, r.Image, r.Source))
+	var i, s, t string
+	for _, v := range r.Ingredients {
+		i += "\n" + v.String()
+	}
+	for _, v := range r.Steps {
+		s += "\n\n" + v
+	}
+	for _, v := range r.Tags {
+		t += " " + v
+	}
+	t = strings.Trim(t, " ")
+	return fmt.Sprintf(fmt.Sprintf("%v\n%v%v\n\n%v\n%v\n%v", r.Title, i, s, t, r.Image, r.Source))
 }
 
 func (r *Recipe) save() error {
-    filename := "data/" + r.Title + ".txt"
-    return ioutil.WriteFile(filename, []byte(r.String()), 0600)
+	filename := "data/" + r.Title + ".txt"
+	b, err := json.Marshal(r)
+	if err != nil {
+		log.Printf("Error: Could not encode Recipe %v to JSON (%v).\n", r.Title, err)
+	}
+	return ioutil.WriteFile(filename, b, 0600)
 }
 
 func loadRecipe(title string) (*Recipe, error) {
-    filename := "data" + title + ".txt"
-    body, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
-    var r *Recipe
-    fmt.Println(body, r)
-    return &Recipe{}, nil
+	filename := "data/" + title + ".txt"
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Printf("Error: Could not read file %v (%v).\n", filename, err)
+		return nil, err
+	}
+	var r Recipe
+	err = json.Unmarshal(b, &r)
+	if err != nil {
+		log.Printf("Error: Could not decode JSON to Recipe %v (%v).\n", title, err)
+		return nil, err
+	}
+	return &r, nil
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world!"))
+	fi, err := ioutil.ReadDir("data/")
+	if err != nil {
+		log.Println(err.Error())
+	}
+	var s []string
+	for _, v := range fi {
+		s = append(s, strings.TrimSuffix(v.Name(), ".txt"))
+	}
+	err = templates.ExecuteTemplate(w, "main.html", s)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	recipe, err := loadRecipe(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", recipe)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	recipe, err := loadRecipe(title)
+	if err != nil {
+		recipe = &Recipe{Title: title}
+	}
+	renderTemplate(w, "edit", recipe)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	newTitle := r.FormValue("title")
+	// Remove `\r` from the ingredients form value?
+	ingredients, err := Ingredients(r.FormValue("ingredients"))
+	if err != nil {
+		log.Fatal("No ingredients found:", err.Error())
+	}
+	var steps []string
+	for _, v := range strings.Split(r.FormValue("steps"), "\n") {
+		v := strings.Trim(v, "\r\n")
+		if v != "" {
+			steps = append(steps, v)
+		}
+	}
+	image := r.FormValue("image")
+	source := r.FormValue("source")
+	tags := strings.Fields(r.FormValue("tags"))
+	for i, v := range tags {
+		if !strings.HasPrefix(v, "#") {
+			tags[i] = "#" + v
+		}
+	}
+	recipe := &Recipe{
+		Title:       newTitle,
+		Ingredients: ingredients,
+		Steps:       steps,
+		Image:       image,
+		Source:      source,
+		Tags:        tags,
+	}
+	if title != newTitle {
+		err = os.Remove("data/" + title + ".txt")
+		if err != nil {
+			log.Printf("Problem removing the file: %v (%v)\n", title, err.Error())
+		}
+	}
+	err = recipe.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+newTitle, http.StatusFound)
+}
+
+func removeHandler(w http.ResponseWriter, r *http.Request, title string) {
+	if err := os.Remove("data/" + title + ".txt"); err != nil {
+		log.Printf("Problem removing the file: %v (%v)\n", title, err.Error())
+	}
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO search for more than one tag, use sets, ...
+	var tags []string
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m != nil {
+		tags = append(tags, m[2])
+	}
+	t := r.FormValue("tags")
+	if t != "" {
+		tags = append(tags, strings.Fields(t)...)
+	}
+	if len(tags) == 0 {
+		http.NotFound(w, r)
+		return
+	}
+	for i, v := range tags {
+		if !strings.HasPrefix(v, "#") {
+			tags[i] = "#" + v
+		}
+	}
+
+	files, err := search.Search("data/", tags[0])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound) // TODO
+	}
+	err = templates.ExecuteTemplate(w, "results.html", files)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	//w.Write([]byte("Not implemented yet."))
+}
+
+var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html", "tmpl/main.html", "tmpl/results.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, r *Recipe) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var validPath = regexp.MustCompile(`^/(edit|save|view|delete|search)/([A-Za-zÄÖÜäöüß0-9\- ]+)$`)
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
 }
 
 func main() {
 	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/delete/", makeHandler(removeHandler))
+	http.HandleFunc("/add/", addHandler)
+	http.HandleFunc("/search/", searchHandler)
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
--- cook_test.go
+++ cook_test.go
@@ -1,104 +1,154 @@
 package main
 
 import (
-    "encoding/json"
-    "testing"
+	"encoding/json"
+	"testing"
 )
 
 func TestIngredientString(t *testing.T) {
-    i := &Ingredient{
-        Name: "Chicken",
-        Amount: 1,
-        Unit: "kg",
-    }
+	cases := []struct {
+		in   Ingredient
+		want string
+	}{
+		{
+			in:   Ingredient{Name: "Chicken", Amount: 1, Unit: "kg"},
+			want: "1kg Chicken",
+		},
+		{
+			in:   Ingredient{Name: "Banana", Amount: 0, Unit: ""},
+			want: "Banana",
+		},
+		{
+			in:   Ingredient{Name: "Cinnamon", Amount: 1.5, Unit: "EL"},
+			want: "1.5EL Cinnamon",
+		},
+	}
 
-    if i.String() != "1 kg Chicken" {
-        t.Error("Expected 1 kg Chicken, got ", i)
-    }
+	for _, c := range cases {
+		got := c.in.String()
+		if got != c.want {
+			t.Errorf("Got %q, want %q", got, c.want)
+		}
+	}
+}
+
+func TestStringIngredient(t *testing.T) {
+	cases := []struct {
+		in   string
+		want []Ingredient
+	}{
+		{
+			in: "1kg Apples\n4TL Honey\n0.5TL Cinnamon",
+			want: []Ingredient{
+				{Name: "Apples", Amount: 1, Unit: "kg"},
+				{Name: "Honey", Amount: 4, Unit: "TL"},
+				{Name: "Cinnamon", Amount: 0.5, Unit: "TL"},
+			},
+		},
+		{
+			in: "1EL Mayo\n\n",
+			want: []Ingredient{
+				{Name: "Mayo", Amount: 1, Unit: "EL"},
+			},
+		},
+		{
+			in: "Banana",
+			want: []Ingredient{
+				{Name: "Banana", Amount: 0, Unit: ""},
+			},
+		},
+        {
+			in: "70g Parmesan\n70g Pinienkerne\r\n1 Knoblauchzehe\r\n100ml Olivenöl\nSalz\n15 getrocknete Tomaten in Öl eingelegt\r\n3EL Fruchtessig Mango\n\n",
+			want: []Ingredient{
+				{Name: "Parmesan", Amount: 70, Unit: "g"},
+				{Name: "Pinienkerne", Amount: 70, Unit: "g"},
+				{Name: "Knoblauchzehe", Amount: 1, Unit: ""},
+				{Name: "Olivenöl", Amount: 100, Unit: "ml"},
+				{Name: "Salz", Amount: 0, Unit: ""},
+				{Name: "getrocknete Tomaten in Öl eingelegt", Amount: 15, Unit: ""},
+				{Name: "Fruchtessig Mango", Amount: 3, Unit: "EL"},
+			},
+        },
+	}
+
+	for _, c := range cases {
+		got, err := Ingredients(c.in)
+		if err != nil {
+			t.Errorf("Ingredients(%q) returned with error: %q", c.in, err.Error())
+		}
+		if !equals(got, c.want) {
+			t.Errorf("Ingredients(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
+func equals(a, b []Ingredient) bool {
+	if a == nil && b == nil {
+		return true
+	}
+
+	if a == nil || b == nil {
+		return false
+	}
+
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
 }
 
 func TestRecipeString(t *testing.T) {
-    r_want := "Chicken\n\n1 kg Chicken\n1 TL Salt\n4 EL Honey\n1 Piece Peperoni\n\n1. Cook chicken.\n\n2. Eat chicken.\n\nchicken.jpg\nhttp://chicken.go/"
-    r_in := &Recipe{
-        Title: "Chicken",
-        Ingredients: []Ingredient{
-            {
-                Name: "Chicken",
-                Amount: 1,
-                Unit: "kg",
-            },
-            {
-                Name: "Salt",
-                Amount: 1,
-                Unit: "TL",
-            },
-            {
-                Name: "Honey",
-                Amount: 4,
-                Unit: "EL",
-            },
-            {
-                Name: "Peperoni",
-                Amount: 1,
-                Unit: "Piece",
-            },
-        },
-        Steps: []string{
-            "1. Cook chicken.",
-            "2. Eat chicken.",
-        },
-        Image: "chicken.jpg",
-        Source: "http://chicken.go/",
-    }
+	r_want := "Chicken\n\n1kg Chicken\n1TL Salt\n4EL Honey\n1Piece Peperoni\n\n1. Cook chicken.\n\n2. Eat chicken.\n\n#Hühnchen #Hauptspeise\nchicken.jpg\nhttp://chicken.go/"
+	r_in := &Recipe{
+		Title: "Chicken",
+		Ingredients: []Ingredient{
+			{Name: "Chicken", Amount: 1, Unit: "kg"},
+			{Name: "Salt", Amount: 1, Unit: "TL"},
+			{Name: "Honey", Amount: 4, Unit: "EL"},
+			{Name: "Peperoni", Amount: 1, Unit: "Piece"},
+		},
+		Steps:  []string{"1. Cook chicken.", "2. Eat chicken."},
+		Image:  "chicken.jpg",
+		Source: "http://chicken.go/",
+        Tags: []string{"#Hühnchen", "#Hauptspeise"},
+	}
 
-    if r_in.String() != r_want {
-        t.Error("Recipe string didn't match expectation.")
-    }
+    if got := r_in.String(); got != r_want {
+        t.Errorf("Got:\n%q\nWant:\n%q", got, r_want)
+	}
 }
 
 func TestJson(t *testing.T) {
-    r_in := &Recipe{
-        Title: "Chicken",
-        Ingredients: []Ingredient{
-            {
-                Name: "Chicken",
-                Amount: 1,
-                Unit: "kg",
-            },
-            {
-                Name: "Salt",
-                Amount: 1,
-                Unit: "TL",
-            },
-            {
-                Name: "Honey",
-                Amount: 4,
-                Unit: "EL",
-            },
-            {
-                Name: "Peperoni",
-                Amount: 1,
-                Unit: "Piece",
-            },
-        },
-        Steps: []string{
-            "1. Cook chicken.",
-            "2. Eat chicken.",
-        },
-        Image: "chicken.jpg",
-        Source: "http://chicken.go/",
-    }
+	r_in := &Recipe{
+		Title: "Chicken",
+		Ingredients: []Ingredient{
+			{Name: "Chicken", Amount: 1, Unit: "kg"},
+			{Name: "Salt", Amount: 1, Unit: "TL"},
+			{Name: "Honey", Amount: 4, Unit: "EL"},
+			{Name: "Peperoni", Amount: 1, Unit: "Piece"},
+		},
+		Steps:  []string{"1. Cook chicken.", "2. Eat chicken."},
+		Image:  "chicken.jpg",
+		Source: "http://chicken.go/",
+	}
 
-    j, err := json.Marshal(r_in)
-    if err != nil {
-        t.Error("Error in the encoding of Recipe: ", err)
-    }
-    var J Recipe
-    err = json.Unmarshal(j, &J)
-    if err != nil {
-        t.Error("Error in the decoding of Recipe: ", err)
-    }
-    if r_in.String() != J.String() {
-        t.Error("Error: Expected value doesn't match the en- and decoded struct.")
-    }
+	j, err := json.Marshal(r_in)
+	if err != nil {
+		t.Error("Error in the encoding of Recipe: ", err)
+	}
+	var J Recipe
+	err = json.Unmarshal(j, &J)
+	if err != nil {
+		t.Error("Error in the decoding of Recipe: ", err)
+	}
+	if r_in.String() != J.String() {
+		t.Error("Error: Expected value doesn't match the en- and decoded struct.")
+	}
 }
